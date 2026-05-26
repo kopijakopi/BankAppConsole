9cffd6c109ae20d810f26d2cc60618be679afd95
Baskaran Kopija <UT011111@unicomtic.com>
2026-05-26 13:40:25 +0530

    BankApplication

--- Bank/Banking Application/obj/Debug/net8.0/Banking Application.AssemblyInfo.cs
+++ Bank/Banking Application/obj/Debug/net8.0/Banking Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Banking Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14739811fda253642149dbfd58092a05d61db23a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Banking Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Banking Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
